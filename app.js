bbcf38353f9435dfb3e6bc77de0a8701a729d4a7
marcosteiner <marco.steiner@windowslive.com>
2019-05-07 22:58:59 +0200

    Configured app.js to use controller for increase and decrease

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ let controller = require('./controller/controller');
 
 app.get('/', (req, res) => controller.get(req, res));
 
-app.post('/increase', (req, res) => {
-    res.json('{"coffee_name":"Milchkaffee","person_name":"Marco"}');
-});
+app.post('/increase', (req, res) => controller.increase(req, res));
+//(req, res) => { res.json('{"coffee_name":"Milchkaffee","person_name":"Marco","coffee_count":29}');}
+
+app.post('/decrease', (req, res) => controller.decrease(req, res));
+//(req, res) => { res.json('{"coffee_name":"Milchkaffee","person_name":"Marco","coffee_count":27}');}
   
 app.listen(3000, () => console.log("Listening on port 3000..."));
