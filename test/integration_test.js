e67ddec1ad7ef0193e38c05e177c77362a3cf945
marcosteiner <marco.steiner@windowslive.com>
2019-05-07 17:20:12 +0200

    Implemented new test for increasing the coffee counter

--- test/integration_test.js
+++ test/integration_test.js
@@ -1,26 +1,48 @@
 var expect  = require("chai").expect;
 var request = require("request");
+let supertest = require("supertest");
+
+let server = supertest.agent("http://localhost:3000");
 
 describe("The Coffee Counter API", function() {
 
-    describe("Get coffee consumption data", function() {
-  
-      var url = "http://localhost:3000/";
-  
-      it("returns status 200", function(done) {
-        request(url, function(error, response, body) {
-          expect(response.statusCode).to.equal(200);
-          done();
-        });
+  describe("Get coffee consumption data", function() {
+
+    var url = "http://localhost:3000/";
+
+    it("returns status 200", function(done) {
+      request(url, function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
       });
-  
-      it("returns the coffee consumption data as json", function(done) {
-        request(url, function(error, response, body) {
-          expect(body).to.equal('[{"coffee_name":"Milchkaffee","person_name":"Marco"},{"coffee_name":"Espresso","person_name":"Marco"},{"coffee_name":"Milchkaffee","person_name":"Pascal"}]');
-          done();
-        });
-      });
-  
     });
-  
+
+    it("returns the coffee consumption data as json", function(done) {
+      request(url, function(error, response, body) {
+        expect(body).to.equal('[{"coffee_name":"Milchkaffee","person_name":"Marco"},{"coffee_name":"Espresso","person_name":"Marco"},{"coffee_name":"Milchkaffee","person_name":"Pascal"}]');
+        done();
+      });
+    });
+
   });
+
+  describe("Increase coffee counter", function() {
+
+    let url = "http://localhost:3000/increase";
+
+    it("returns status 200", (done) => {
+      server.post(url)
+      .send('{"coffee_name":"Milchkaffee","person_name":"Marco"}')
+      .expect("Content-type",/json/)
+      .expect(200)
+      .end(function(err,res){
+        res.status.should.equal(200);
+        res.body.error.should.equal(false);
+        res.body.data.should.equal(30);
+        done();
+      });
+    });
+
+  });
+  
+});
