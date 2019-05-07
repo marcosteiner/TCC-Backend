ab41d5c89d0bd69ebd5f763e2b8bcc3c9605b329
marcosteiner <marco.steiner@windowslive.com>
2019-05-07 15:36:30 +0200

    Created integration_test and updated test runner config

---
+++ test/integration_test.js
@@ -0,0 +1,27 @@
+var expect  = require("chai").expect;
+var request = require("request");
+const dbs = require("../services/db_service")
+
+describe("The Coffee Counter API", function() {
+
+    describe("Get coffee consumption data", function() {
+  
+      var url = "http://localhost:3000/";
+  
+      it("returns status 200", function(done) {
+        request(url, function(error, response, body) {
+          expect(response.statusCode).to.equal(200);
+          done();
+        });
+      });
+  
+      it("returns the coffee consumption data as json", function(done) {
+        request(url, function(error, response, body) {
+          expect(body).to.equal('[{"coffee_name":"Milchkaffee","person_name":"Marco"},{"coffee_name":"Espresso","person_name":"Marco"},{"coffee_name":"Milchkaffee","person_name":"Pascal"}]');
+          done();
+        });
+      });
+  
+    });
+  
+  });
